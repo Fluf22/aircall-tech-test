46c275d909b128c8e9f806f32bd9a758271a25c5
Fluf22 <Fluf22@users.noreply.github.com>
2022-01-09 14:32:58 +0000

    Add endpoints.

--- src/services/calls.ts
+++ src/services/calls.ts
@@ -9,3 +9,24 @@ export const fetchCalls = (token: string, offset: number = 0, limit: number = 10
             console.error("Error while trying to fetch calls: ", err);
         });
 };
+
+export const addNote = (token: string, callId: string, content: string) => {
+    const headers = { "Authorization": `Bearer ${token}`, "content-type": "application/json" };
+    const body = JSON.stringify({ content });
+    return fetch(`${API_BASE_URL}/calls/${callId}/note`, { method: "POST", headers, body })
+        .then(checkStatus)
+        .then(parseJSON)
+        .catch(err => {
+            console.error(`Error while trying to add a note to a call (ID: ${callId}): `, err);
+        });
+};
+
+export const triggerArchiveStatus = (token: string, callId: string) => {
+    const headers = { "Authorization": `Bearer ${token}` };
+    return fetch(`${API_BASE_URL}/calls/${callId}/archive`, { method: "PUT", headers })
+        .then(checkStatus)
+        .then(parseJSON)
+        .catch(err => {
+            console.error(`Error while trying to trigger archive status on a call (ID: ${callId}): `, err);
+        });
+};
