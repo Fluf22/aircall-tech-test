297181b53aef2c889a7353bb431157fee1d0f9fd
Thomas Raffray <Fluf22@users.noreply.github.com>
2023-09-08 14:34:20 +0200

    Update API_BASE_URL and PUSHER_APP_AUTH_ENDPOINT constant values
    The API_BASE_URL and PUSHER_APP_AUTH_ENDPOINT constants have been changed to point to the development server instead of production. This change aims to prevent accidental disruptions to the production environment during testing or development.

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,8 +1,8 @@
-
-export const API_BASE_URL: string = "https://frontend-test-api.aircall.io";
+export const API_BASE_URL: string = "https://frontend-test-api.aircall.dev";
 export const PUSHER_APP_KEY: string = "d44e3d910d38a928e0be";
 export const PUSHER_APP_CLUSTER: string = "eu";
-export const PUSHER_APP_AUTH_ENDPOINT: string = "https://frontend-test-api.aircall.io/pusher/auth";
+export const PUSHER_APP_AUTH_ENDPOINT: string =
+  "https://frontend-test-api.aircall.dev/pusher/auth";
 export const PUSHER_CHANNEL_NAME: string = "private-aircall";
 export const PUSHER_CHANNEL_EVENT: string = "update-call";
 export const ITEMS_PER_PAGE: number = 10;
